02193403481d82db31cda7334239048e9aef9061
Carlos E Silva <kadu864@gmail.com>
2015-06-07 16:20:21 -0400

    Installed browsersync into project

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,34 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var plumber     = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            // directory: true
+        },
+        open: "ui"
+    });
+
+    gulp.watch("./sass/**/*.scss", ['sass']);
+    gulp.watch(["./*.html", "./js/**/*.js"]).on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    gulp.src('./sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css'));
+    return gulp.src("./sass/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
-	gulp.watch('./sass/**/*.scss', ['sass']);
-});
+
+
+// Default task
+gulp.task('default', ['serve']);
