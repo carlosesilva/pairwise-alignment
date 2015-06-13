f4e4577acf5cc4354ea2b1f0b0d4ee4165a6e552
Carlos E Silva <kadu864@gmail.com>
2015-06-13 12:59:22 -0400

    Improved input validation for pos/neg integers

--- js/build-matrix.js
+++ js/build-matrix.js
@@ -32,7 +32,7 @@ function buildDynamicProgrammingMatrix(seq1, seq2){
         result += '<tr>';
         for (var j = -1; j<seq2.length; j++){
             result += '<td>';
-            result += '<input type="text" class="dynamicProgrammingMatrixCell" i="' + i + '" j="' + j + '"/>';
+            result += '<input type="text" class="dynamicProgrammingMatrixCell" pattern="^-?\\d+" i="' + i + '" j="' + j + '"/>';
             result += '</td>';
         }
         result += '</tr>';
--- js/scratch.js
+++ js/scratch.js
@@ -1,67 +1,125 @@
 $(document).ready(function(){
-	
-
-	/**
-	 * Initialize sequences
-	 */
-	var sequence1 = 'THIS',
-		sequence2 = 'THAT',
-		matchScore = 3,
-		mismatchScore = 0,
-		gapPenalty = -5,
-		mode = 'global';
-
-	/**
-	 * Build input matrix
-	 */
-	$('#inputTableContainer').html(buildDynamicProgrammingMatrixWrapper(sequence1, sequence2));
-
-	
-	/**
-	 * Read dynamicProgrammingMatrix when button is clicked
-	 */
-	$('#readMatrix').click(function(event) {
-		console.log(dynamicProgrammingMatrixRead($('#inputTableContainer .dynamicProgrammingMatrix')));
-	});
 
 
+    /*=================================
+    =            Variables            =
+    =================================*/
+    var sequence1 = 'THIS',
+    sequence2 = 'THAT',
+    matchScore = 3,
+    mismatchScore = 0,
+    gapPenalty = -5,
+    mode = 'global',
+    instantFeedback = false;
+    
+    
+    /*-----  End of Variables  ------*/
 
-	// calculate correct matrix array
-	var correctMatrix = computeMatrix(mode,sequence1,sequence2,matchScore,mismatchScore,gapPenalty)
 
-	// create div for testing print results 
-	$(document.createElement('div')).attr('id', 'correctMatrix').addClass('dynamicProgrammingMatrixWrapper').html(buildDynamicProgrammingMatrixWrapper(sequence1, sequence2)).appendTo('body');
+    
+    
 
-	// build table skeleton
-	$('#correctMatrix').html(buildDynamicProgrammingMatrixWrapper(sequence1, sequence2));
-	
-	// print correct matrix to a table
-	printMatrix(correctMatrix,$('#correctMatrix').find('.dynamicProgrammingMatrix'));
+
+    // calculate correct matrix array
+    var correctMatrix = computeMatrix(mode,sequence1,sequence2,matchScore,mismatchScore,gapPenalty)
 
 
 
-	// compare matrices
-	$('#compareMatrices').click(function(event) {
-		var inputMatrix = dynamicProgrammingMatrixRead($('#inputTableContainer .dynamicProgrammingMatrix'));
-		displayFeedback($('#inputTableContainer .dynamicProgrammingMatrix'), compareMatrices(correctMatrix, inputMatrix));
-	});
 
-	// limit input to dynamicProgrammingMatrixCell to numbers and dashes
-	$('.dynamicProgrammingMatrixCell').keyup(function(event) {
-		$(this).val($(this).val().replace(/[^-?0-9]/, ''));
-		// Remember to add, replace any dashes that are in anyposition other than first
-	});
 
-	$('#inputTableContainer .dynamicProgrammingMatrixCell').focusout(function(event) {
-		var inputMatrix = dynamicProgrammingMatrixRead($('#inputTableContainer .dynamicProgrammingMatrix'));
-		displayFeedback($('#inputTableContainer .dynamicProgrammingMatrix'), compareMatrices(correctMatrix, inputMatrix));
-		// // validate input
-		// if ($(this).val() != ''  && !$.isNumeric($(this).val()))
-		// 	$(this).addClass('wrong');
-	});
 
-	// display feedback
-		
+    /*=============================
+    =            UI/UX            =
+    =============================*/
+    // Build input matrix
+    $('#inputTableContainer').html(buildDynamicProgrammingMatrixWrapper(sequence1, sequence2));
+
+    
+
+    // limit input to dynamicProgrammingMatrixCell to positive and negative integers only
+    $('#inputTableContainer .dynamicProgrammingMatrixCell').keyup(function(event) {
+        $(this).val(filterInteger($(this).val()));
+    });
+
+
+
+    // compare matrices when the compare button is cliked
+    // this makes more sense when instant feedback is turned off or when in TEST mode
+    $('#compareMatrices').click(function(event) {
+        var inputMatrix = dynamicProgrammingMatrixRead($('#inputTableContainer .dynamicProgrammingMatrix'));
+        displayFeedback($('#inputTableContainer .dynamicProgrammingMatrix'), compareMatrices(correctMatrix, inputMatrix));
+    });
+    
+    /*-----  End of UI/UX  ------*/
+    
+
+
+
+    // Instant feedback
+    if (instantFeedback){
+        $('#inputTableContainer .dynamicProgrammingMatrixCell').focusout(function(event) {
+            // validate input again in case .keyup() failed e.g. value was pasted in, dragged in
+            $(this).val(filterInteger($(this).val()));
+
+            var inputMatrix = dynamicProgrammingMatrixRead($('#inputTableContainer .dynamicProgrammingMatrix'));
+            displayFeedback($('#inputTableContainer .dynamicProgrammingMatrix'), compareMatrices(correctMatrix, inputMatrix));
+        });
+    }
+    
+
+
+
+
+
+
+    /*===============================
+    =            TESTING            =
+    ===============================*/
+    // create div for testing print results 
+    $(document.createElement('div')).attr('id', 'correctMatrix').addClass('dynamicProgrammingMatrixWrapper').html(buildDynamicProgrammingMatrixWrapper(sequence1, sequence2)).appendTo('body');
+
+    // build correct table skeleton
+    $('#correctMatrix').html(buildDynamicProgrammingMatrixWrapper(sequence1, sequence2));
+    
+    // print correct matrix to a table
+    printMatrix(correctMatrix,$('#correctMatrix').find('.dynamicProgrammingMatrix'));
+    
+    
+
+
+    // Read dynamicProgrammingMatrix when button is clicked
+    $('#readMatrix').click(function(event) {
+        console.log(dynamicProgrammingMatrixRead($('#inputTableContainer .dynamicProgrammingMatrix')));
+    });
+    /*-----  End of TESTING  ------*/
+    
+    
+
+
+
+
+
+
+
 
 
 });
+
+
+/*=================================
+=            Functions            =
+=================================*/
+
+
+function filterInteger(val){
+    return (val[0] === '-') ? ('-' + val.replace(/[^0-9]/g, '')) : (val.replace(/[^0-9]/g, ''));
+    // use below direcly on html element's onkeyup attr
+    //  this.value = (this.value[0] === '-') ? ('-' + this.value.replace(/[^0-9]/g, '')) : (this.value.replace(/[^0-9]/g, ''));
+}
+
+function evaluate (argument) {
+    // body...
+}
+
+
+/*-----  End of Functions  ------*/
