5e27345e6ad5b290089b886761389ec00d9b537f
Web Intern <webintern@mirandacreative.com>
2015-06-17 16:22:56 -0400

    Modified readMatrix to read traceback info

--- js/read-matrix.js
+++ js/read-matrix.js
@@ -9,9 +9,14 @@ function readMatrix (dynamicProgrammingMatrix) {
     dynamicProgrammingMatrix.find('tr').each(function() {
         var row = [];
         $(this).find('td').each(function() {
+            // prepare traceback. convert to binary then explode to array
+            var traceback = parseInt($(this).find('.tracebackSelect').val()).toString(2);
+            if (traceback.length < 4) traceback = Array(4 - traceback.length + 1 ).join("0") + traceback;
+            traceback = traceback.split("");
+            
             row.push({
                 score: parseInt($(this).find('.dynamicProgrammingMatrixCell').val(), 10),
-                traceback: [false, false, false, false]
+                traceback: traceback
             });
         });
         matrix.push(row);
